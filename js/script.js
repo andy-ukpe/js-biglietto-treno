c15d4e86a30b7ed74afcf04a36905caba563725c
andy <andy.ukpe@outlook.com>
2020-05-20 17:18:55 +0200

    completato la logica di calcolo

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// kilometraggio
+var kilometri = parseInt(prompt("Inserire i km da percorrere"));
+console.log(kilometri);
+
+// prezzo del biglietto
+var prezzoBiglietto = kilometri * 0.21;
+console.log(prezzoBiglietto);
+var prezzoUnitario = '€' + prezzoBiglietto;
+console.log(prezzoUnitario);
+
+
+// età del viaggiatore
+var etaViaggiatore = (prompt("Inserire età del passegiero"))
+console.log(etaViaggiatore);
+
+// operatori logici
+if ( etaViaggiatore <= 17 ){
+  var scontoMinori =  (prezzoBiglietto * 20) / 100 ;
+  console.log(scontoMinori);
+  var calcoloSconto = prezzoBiglietto - scontoMinori;
+  console.log(calcoloSconto);
+  var prezzoScontato = '€' + calcoloSconto;
+  console.log(prezzoScontato);
+  document.getElementById('prezzo-unitario').innerHTML = 'Prezzo Unitario: ' + prezzoUnitario ;
+  document.getElementById('prezzo-scontato').innerHTML = 'Prezzo Under 17: ' + prezzoScontato ;
+} else if (etaViaggiatore >= 65){
+  var scontoMajor = ( prezzoBiglietto * 40 ) / 100;
+  console.log(scontoMajor);
+  var calcoloSconto = (prezzoBiglietto - scontoMajor);
+  console.log(calcoloSconto);
+  var prezzoScontato = '€' + calcoloSconto;
+  console.log(prezzoScontato);
+  document.getElementById('prezzo-unitario').innerHTML= 'Prezzo Unitario: ' + prezzoUnitario ;
+  document.getElementById('prezzo-scontato').innerHTML= 'Prezzo Senior: ' + prezzoScontato ;
+} else {
+  console.log(prezzoUnitario);
+  document.getElementById('prezzo-unitario').innerHTML= 'Prezzo Unitario: ' + prezzoUnitario ;
+}
